bf05b059ef21300fb6432aaeb22855d08562954e
zhou_zm <zm_zhou1998@163.com>
2023-11-26 21:04:47 +0800

    add my q learing demo

--- gridWorldGame.py
+++ gridWorldGame.py
@@ -64,48 +64,58 @@ class Grid: # Environment
     return set(self.actions.keys()) | set(self.rewards.keys())
 
 
-def standard_grid():
-  # define a grid that describes the reward for arriving at each state
-  # and possible actions at each state
-  # the grid looks like this
-  # x means you can't go there
-  # s means start position
-  # number means reward at that state
-  # .  .  .  1
-  # .  x  . -1
-  # s  .  .  .
-  g = Grid(3, 4, (2, 0))
-  rewards = {(0, 3): 1, (1, 3): -1}
-  actions = {
-    (0, 0): ('D', 'R'),
-    (0, 1): ('L', 'R'),
-    (0, 2): ('L', 'D', 'R'),
-    (1, 0): ('U', 'D'),
-    (1, 2): ('U', 'D', 'R'),
-    (2, 0): ('U', 'R'),
-    (2, 1): ('L', 'R'),
-    (2, 2): ('L', 'R', 'U'),
-    (2, 3): ('L', 'U'),
-  }
-  g.set(rewards, actions)
-  return g
+def standard_grid(st_pos=(0, 0), end_pos=(3, 3), barrier_num=1 , h=5, w=5):
+    # define a grid that describes the reward for arriving at each state
+    # and possible actions at each state
+    # the grid looks like this
+    # x means you can't go there
+    # s means start position
+    # number means reward at that state
+    # .  .  .  1
+    # .  x  . -1
+    # s  .  .  .
+    g = Grid(h, w, st_pos)
+    rewards = {end_pos: 1}
+    for _ in range(barrier_num):
+      while True:
+        _tmp_b = (np.random.randint(0, h), np.random.randint(0, w))
+        if _tmp_b not in rewards:
+          rewards[_tmp_b] = -1
+          break
+
+    actions = {}
+    for i in range(h):
+      for j in range(w):
+        if (i, j) in rewards:
+          continue
+        actions[(i, j)] = []
+        if i == 0:
+          actions[(i, j)].append('D')
+        elif i == h - 1:
+          actions[(i, j)].append('U')
+        else:
+          actions[(i, j)].append('U')
+          actions[(i, j)].append('D')
+        
+        if j == 0:
+          actions[(i, j)].append('R')
+        elif j == w - 1:
+          actions[(i, j)].append('L')
+        else:
+          actions[(i, j)].append('L')
+          actions[(i, j)].append('R')
+    g.set(rewards, actions)
+    return g
 
 
-def negative_grid(step_cost=-0.1):
+def negative_grid(st_pos=(0, 0), end_pos=(3, 3), barrier_num=3 , h=5, w=5, step_cost=-0.1):
   # in this game we want to try to minimize the number of moves
   # so we will penalize every move
-  g = standard_grid()
-  g.rewards.update({
-    (0, 0): step_cost,
-    (0, 1): step_cost,
-    (0, 2): step_cost,
-    (1, 0): step_cost,
-    (1, 2): step_cost,
-    (2, 0): step_cost,
-    (2, 1): step_cost,
-    (2, 2): step_cost,
-    (2, 3): step_cost,
-  })
+  g = standard_grid(st_pos=st_pos, end_pos=end_pos, barrier_num=barrier_num , h=h, w=w)
+  for i in range(h):
+    for j in range(w):
+      if (i, j) not in g.rewards and (i, j) != (g.i, g.j):
+        g.rewards[(i, j)] = step_cost
   return g
 
 
